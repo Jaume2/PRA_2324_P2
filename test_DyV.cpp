0d4d0fcb2dee6dd4e5e3493c258dced4492617ab
Jaume Gimeno Ponz <jgimpon@upv.edu.es>
2025-11-09 19:20:46 +0100

    Add files via upload

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main(){
 	vector<int> v = {1,2,3,4,5,6,7,8,9,10};
-	BusquedaBinaria(2, v, 1, 10);
+	int res = BusquedaBinaria(2, v, 0, 9);
+	cout << " " << res << endl;
+	int res2 = QuickSort(v, 0, 9);
+	cout << " " << res2 <<endl;
 	return 0;
 }
